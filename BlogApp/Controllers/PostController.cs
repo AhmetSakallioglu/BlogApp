e94be0fd193c47b652f49354c7972acca08005f9
Ahmet Sakallıoğlu <ahmetsakallioglu54@gmail.com>
2024-07-27 03:33:41 +0300

    update

--- BlogApp/Controllers/PostController.cs
+++ BlogApp/Controllers/PostController.cs
@@ -1,8 +1,10 @@
-﻿using BlogApp.Entities;
+﻿using System.ComponentModel.DataAnnotations;
 using BlogApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Threading.Tasks;
+using BlogApp.Entities;
 
 namespace BlogApp.Controllers
 {
@@ -10,10 +12,13 @@ namespace BlogApp.Controllers
 	{
 		private readonly DatabaseContext _databaseContext;
 		private readonly IConfiguration _configuration;
-		public PostController(DatabaseContext databaseContext, IConfiguration configuration)
+		private readonly IWebHostEnvironment _environment;
+
+		public PostController(DatabaseContext databaseContext, IConfiguration configuration, IWebHostEnvironment environment)
 		{
 			_databaseContext = databaseContext;
 			_configuration = configuration;
+			_environment = environment;
 		}
 
 		public IActionResult Index()
@@ -27,7 +32,48 @@ namespace BlogApp.Controllers
 			return View();
 		}
 
+		[HttpPost]
+		public async Task<IActionResult> Create(CreatePostViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var post = new Post
+				{
+					Title = model.Title,
+					Content = model.Content,
+					Date = DateTime.Now,
+					IsActive = model.IsActive,
+					PostTag = model.TagId.Select(tagId => new PostTag { TagId = tagId }).ToList(),
+					PostImage = new List<PostImage>()
+				};
+
+				if (model.Images != null && model.Images.Count > 0)
+				{
+					foreach (var image in model.Images)
+					{
+						var fileName = Path.GetFileName(image.FileName);
+						var filePath = Path.Combine(_environment.WebRootPath, "uploads", fileName);
+
+						using (var stream = new FileStream(filePath, FileMode.Create))
+						{
+							await image.CopyToAsync(stream);
+						}
+
+						post.PostImage.Add(new PostImage
+						{
+							Image = new Image { ImageUrl = "/uploads/" + fileName }
+						});
+					}
+				}
+
+				await _databaseContext.Post.AddAsync(post);
+				await _databaseContext.SaveChangesAsync();
+
+				return RedirectToAction(nameof(Index));
+			}
+
+			ViewBag.Tags = _databaseContext.Tag.ToList();
+			return View(model);
+		}
 	}
 }
-
-
---
+++ BlogApp/Models/PostViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Http;
+
+namespace BlogApp.Models
+{
+    public class CreatePostViewModel
+    {
+        [Required(ErrorMessage = "Başlık girmek zorunludur.")]
+        [StringLength(255, ErrorMessage = "Başlık 255 karakteri geçmemelidir.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "İçerik girmek zorunludur.")]
+        public string Content { get; set; }
+
+        public bool IsActive { get; set; } = false;
+
+        public List<int> TagId { get; set; }
+
+        public List<IFormFile> Images { get; set; }
+
+        public List<SelectListItem> Tags { get; set; }
+    }
+
+}
